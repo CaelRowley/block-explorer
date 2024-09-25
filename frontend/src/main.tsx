3b0d1767370bf25c6666dfefcc4362b2753cb07d
Cael Rowley <drdeus@pm.me>
2024-09-25 12:53:55 +0200

    feat: add sortable fields to table

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <div style={{ marginLeft: '10%', marginRight: '10%', marginTop: '40px' }}>
     <App />
+
+    </div>
   </StrictMode>,
 );
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -9,6 +9,7 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  TableSortLabel,
   CircularProgress,
   Pagination,
   Grid2 as Grid,
@@ -25,28 +26,33 @@ type Block = {
 
 const Home: React.FC = () => {
   const pageSize = 10;
-  const orderByField = "timestamp";
-  const orderByDirection = "desc";
   const refetchInterval = 15000;
 
   const [page, setPage] = React.useState<number>(1);
   const onHandlePagination = (_: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
+  const [orderByField, setOrderByField] = React.useState<string>("timestamp");
+  const [orderByDirection, setOrderByDirection] = React.useState<
+    "asc" | "desc"
+  >("desc");
+  const handleOnSortClick = (property: string) => {
+    const isAsc = orderByField === property && orderByDirection === "asc";
+    setOrderByDirection(isAsc ? "desc" : "asc");
+    setOrderByField(property);
+  };
 
   const { isPending, error, data } = useQuery({
-    queryKey: [page],
+    queryKey: [page, orderByField, orderByDirection],
     queryFn: () =>
       fetch(
         `${import.meta.env.VITE_BACKEND_URL}/eth/blocks?page=${page}&pageSize=${pageSize}&orderByField=${orderByField}&orderByDirection=${orderByDirection}`,
       ).then((res) => res.json()),
     refetchInterval,
   });
-
   if (isPending) {
     return <CircularProgress />;
   }
-
   if (error) {
     return <p>Oops something went wrong...</p>;
   }
@@ -55,15 +61,55 @@ const Home: React.FC = () => {
     <Paper sx={{ padding: "32px" }}>
       <h2>Blocks Data</h2>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: "100%" }} aria-label="simple table">
+        <Table sx={{ minWidth: "100%" }} aria-label="Block table">
           <TableHead>
             <TableRow>
-              <TableCell align="left">Number</TableCell>
-              <TableCell align="right">Size</TableCell>
-              <TableCell align="right">Timestamp</TableCell>
-              <TableCell align="right">Nonce</TableCell>
-              <TableCell align="right">Gas&nbsp;Limit</TableCell>
-              <TableCell align="right">Hash</TableCell>
+              <TableCell align="left">
+                <TableSortLabel
+                  active={orderByField === "number"}
+                  direction={
+                    orderByField === "number" ? orderByDirection : "asc"
+                  }
+                  onClick={() => handleOnSortClick("number")}
+                >
+                  <h3>Number</h3>
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="right">
+                <TableSortLabel
+                  active={orderByField === "size"}
+                  direction={orderByField === "size" ? orderByDirection : "asc"}
+                  onClick={() => handleOnSortClick("size")}
+                >
+                  <h3>Size</h3>
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="right">
+                <TableSortLabel
+                  active={orderByField === "timestamp"}
+                  direction={
+                    orderByField === "timestamp" ? orderByDirection : "asc"
+                  }
+                  onClick={() => handleOnSortClick("timestamp")}
+                >
+                  <h3>Timestamp</h3>
+                </TableSortLabel>
+              </TableCell>
+              <TableCell align="right">
+                <h3>Nonce</h3>
+              </TableCell>
+              <TableCell align="right">
+                <h3>Gas&nbsp;Limit</h3>
+              </TableCell>
+              <TableCell align="right">
+                <TableSortLabel
+                  active={orderByField === "hash"}
+                  direction={orderByField === "hash" ? orderByDirection : "asc"}
+                  onClick={() => handleOnSortClick("hash")}
+                >
+                  <h3>Hash</h3>
+                </TableSortLabel>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
