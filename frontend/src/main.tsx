c8470720623cd39191fbad184a2b85a938aaaea9
Cael Rowley <drdeus@pm.me>
2024-09-25 13:19:01 +0200

    feat: add deleting of selected blocks or all blocks in db

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div style={{ marginLeft: '10%', marginRight: '10%', marginTop: '40px' }}>
-    <App />
-
+    <div style={{ marginLeft: "10%", marginRight: "10%", marginTop: "40px" }}>
+      <App />
     </div>
   </StrictMode>,
 );
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,6 +1,7 @@
-import * as React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Decimal from "decimal.js";
+import axios from "axios";
 import {
   Paper,
   TableContainer,
@@ -13,6 +14,8 @@ import {
   CircularProgress,
   Pagination,
   Grid2 as Grid,
+  Checkbox,
+  Button,
 } from "@mui/material";
 
 type Block = {
@@ -25,28 +28,66 @@ type Block = {
 };
 
 const Home: React.FC = () => {
+  const queryClient = useQueryClient();
   const pageSize = 10;
   const refetchInterval = 15000;
 
-  const [page, setPage] = React.useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
+  const [orderByField, setOrderByField] = useState<string>("timestamp");
+  const [orderByDir, setOrderByDir] = useState<"asc" | "desc">("desc");
+  const [isAllChecked, setIsAllChecked] = useState<boolean>(false);
+
   const onHandlePagination = (_: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
-  const [orderByField, setOrderByField] = React.useState<string>("timestamp");
-  const [orderByDirection, setOrderByDirection] = React.useState<
-    "asc" | "desc"
-  >("desc");
+
   const handleOnSortClick = (property: string) => {
-    const isAsc = orderByField === property && orderByDirection === "asc";
-    setOrderByDirection(isAsc ? "desc" : "asc");
+    const isAsc = orderByField === property && orderByDir === "asc";
+    setOrderByDir(isAsc ? "desc" : "asc");
     setOrderByField(property);
   };
 
+  const handleRowChecked = (key: string) => {
+    setIsAllChecked(false);
+    if (selectedRows.includes(key)) {
+      setSelectedRows(selectedRows.filter((item) => item !== key));
+    } else {
+      setSelectedRows([...selectedRows, key]);
+    }
+  };
+  
+  const handleAllChecked = () => {
+    setIsAllChecked(!isAllChecked);
+    setSelectedRows([]);
+  };
+
+  const handleOnDeleteClick = async () => {
+    try {
+      if (isAllChecked) {
+        await axios.delete(
+          `${import.meta.env.VITE_BACKEND_URL}/eth/blocks/all`,
+        );
+      } else {
+        await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/eth/blocks`, {
+          data: {
+            hashes: selectedRows,
+          },
+        });
+      }
+      setSelectedRows([]);
+      setIsAllChecked(false);
+      await queryClient.refetchQueries();
+    } catch (error) {
+      console.error("Error deleting blocks:", error);
+    }
+  };
+
   const { isPending, error, data } = useQuery({
-    queryKey: [page, orderByField, orderByDirection],
+    queryKey: [page, orderByField, orderByDir],
     queryFn: () =>
       fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/eth/blocks?page=${page}&pageSize=${pageSize}&orderByField=${orderByField}&orderByDirection=${orderByDirection}`,
+        `${import.meta.env.VITE_BACKEND_URL}/eth/blocks?page=${page}&pageSize=${pageSize}&orderByField=${orderByField}&orderByDirection=${orderByDir}`,
       ).then((res) => res.json()),
     refetchInterval,
   });
@@ -64,12 +105,17 @@ const Home: React.FC = () => {
         <Table sx={{ minWidth: "100%" }} aria-label="Block table">
           <TableHead>
             <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  color="primary"
+                  checked={isAllChecked}
+                  onChange={() => handleAllChecked()}
+                />
+              </TableCell>
               <TableCell align="left">
                 <TableSortLabel
                   active={orderByField === "number"}
-                  direction={
-                    orderByField === "number" ? orderByDirection : "asc"
-                  }
+                  direction={orderByField === "number" ? orderByDir : "asc"}
                   onClick={() => handleOnSortClick("number")}
                 >
                   <h3>Number</h3>
@@ -78,7 +124,7 @@ const Home: React.FC = () => {
               <TableCell align="right">
                 <TableSortLabel
                   active={orderByField === "size"}
-                  direction={orderByField === "size" ? orderByDirection : "asc"}
+                  direction={orderByField === "size" ? orderByDir : "asc"}
                   onClick={() => handleOnSortClick("size")}
                 >
                   <h3>Size</h3>
@@ -87,9 +133,7 @@ const Home: React.FC = () => {
               <TableCell align="right">
                 <TableSortLabel
                   active={orderByField === "timestamp"}
-                  direction={
-                    orderByField === "timestamp" ? orderByDirection : "asc"
-                  }
+                  direction={orderByField === "timestamp" ? orderByDir : "asc"}
                   onClick={() => handleOnSortClick("timestamp")}
                 >
                   <h3>Timestamp</h3>
@@ -104,7 +148,7 @@ const Home: React.FC = () => {
               <TableCell align="right">
                 <TableSortLabel
                   active={orderByField === "hash"}
-                  direction={orderByField === "hash" ? orderByDirection : "asc"}
+                  direction={orderByField === "hash" ? orderByDir : "asc"}
                   onClick={() => handleOnSortClick("hash")}
                 >
                   <h3>Hash</h3>
@@ -114,8 +158,20 @@ const Home: React.FC = () => {
           </TableHead>
           <TableBody>
             {data.data.map((row: Block) => {
+              const isRowSelected = selectedRows.includes(row.hash);
               return (
-                <TableRow sx={{ height: 60 }} key={row.hash}>
+                <TableRow
+                  sx={{ height: 60 }}
+                  key={row.hash}
+                  selected={isRowSelected || isAllChecked}
+                >
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      color="primary"
+                      onChange={() => handleRowChecked(row.hash)}
+                      checked={isRowSelected || isAllChecked}
+                    />
+                  </TableCell>
                   <TableCell align="left">{row.number.toString()}</TableCell>
                   <TableCell align="right">{row.size.toString()}</TableCell>
                   <TableCell align="right">
@@ -146,13 +202,24 @@ const Home: React.FC = () => {
         </Table>
       </TableContainer>
       <br />
-      <Grid display="flex" justifyContent="end">
-        <Pagination
-          page={page}
-          onChange={onHandlePagination}
-          count={Math.ceil(data.count / pageSize)}
-          color="primary"
-        />
+      <Grid container justifyContent={"space-between"} spacing={2}>
+        <Grid display="flex" justifyContent="end">
+          <Button
+            onClick={handleOnDeleteClick}
+            disabled={selectedRows.length <= 0 && !isAllChecked}
+            variant="outlined"
+          >
+            Delete Selected
+          </Button>
+        </Grid>
+        <Grid display="flex" justifyContent="end">
+          <Pagination
+            page={page}
+            onChange={onHandlePagination}
+            count={Math.ceil(data.count / pageSize)}
+            color="primary"
+          />
+        </Grid>
       </Grid>
     </Paper>
   );
