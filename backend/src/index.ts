e30e9c6bd7efeaa326fd80163409892413c7d19f
Cael Rowley <drdeus@pm.me>
2024-09-25 11:02:27 +0200

    feat: add poll for latest block

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import { startServer } from "src/server";
+import { startPoll } from "src/poll";
 
 const PORT = Number(process.env.PORT!);
+const NODE_URL = process.env.NODE_URL!;
 
 startServer(PORT).catch((error) => {
   console.error(error);
 });
+
+startPoll(NODE_URL, 15000)
--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -4,14 +4,7 @@ import bodyParser from "koa-bodyparser";
 
 import router from "src/routes";
 
-import { fetchBlock } from "src/eth/client";
-import { insertBlock } from "src/db/block";
-
 export const startServer = async (port: number) => {
-  const block = await fetchBlock(process.env.NODE_URL!);
-  const dbBlock = await insertBlock(block);
-  console.log(dbBlock);
-
   const app = new Koa();
 
   app.use(cors());
